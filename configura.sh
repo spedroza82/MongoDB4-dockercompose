ee82ca2819d5fb251a42ea58562d9da4536c8c50
spedroza82 <sistemas.elrosario@gmail.com>
2019-05-22 19:59:29 -0500

    Create configura.sh

---
+++ configura.sh
@@ -0,0 +1,8 @@
+echo "==================== Configuracion configServers replica set\n"
+docker exec -it mongocfg1 bash -c "echo 'rs.initiate({_id: \"mongors1conf\",configsvr: true, members: [{ _id : 0, host : \"mongocfg1\" },{ _id : 1, host : \"mongocfg2\" }, { _id : 2, host : \"mongocfg3\" }]})' | mongo"
+echo "==================== Construccion de shard replica set\n"
+docker exec -it mongors1n1 bash -c "echo 'rs.initiate({_id : \"mongors1\", members: [{ _id : 0, host : \"mongors1n1\" },{ _id : 1, host : \"mongors1n2\" },{ _id : 2, host : \"mongors1n3\" }]})' | mongo"
+echo "==================== Agregar a mongos\n"
+docker exec -it mongos1 bash -c "echo 'sh.addShard(\"mongors1/mongors1n1\")' | mongo "
+echo "==================== Check\n"
+docker exec -it mongos1 bash -c "echo 'sh.status()' | mongo "
